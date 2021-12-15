131c98f324260efc08818d68d2ca9fc9acf5eeac
Andrey Cunha <50552013+Andreycb@users.noreply.github.com>
2021-12-15 20:50:12 -0300

    Delete settings.py

--- settings.py
+++
@@ -1,8 +0,0 @@
-import os
-from dotenv import load_dotenv, find_dotenv
-
-load_dotenv(find_dotenv())
-
-ROOT_PROJECT = os.path.dirname(os.path.abspath("__file__"))
-CHROME_PATH = os.getenv('CHROME_PATH', 'chromedriver')
-USE_CHROME_HEADLESS = os.getenv('USE_CHROME_HEADLESS', '0') == '1'
